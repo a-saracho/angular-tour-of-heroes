6f6835ea83efb0e11b64c7256578ff67829d579a
Ainara Saracho <ainara.saracho@gmail.com>
2021-03-17 19:26:27 +0100

    Crear componente hero

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+/* Importa clases y componentes que va a utilizar el componente*/
+import { Component, OnInit } from '@angular/core';
+
+/* Metadatos del componente
+  * string selector nombre del selector, se utilizará esta etiqueta html para llamar al componente
+  * string templateUrl fichero html del componente
+  * array styleUrls estilos css del componente
+*/
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+/* Se exporta el componente para poder utilizarlo fuera de si mismo */
+export class HeroesComponent implements OnInit {
+
+  /*Propiedades del componente*/
+  hero = 'Windstorm';
+
+
+  constructor() { /* Constructor del componente */}
+
+  ngOnInit(): void {
+    /*Código que se ejecuta al crear el componente con éxito*/
+  }
+
+}
