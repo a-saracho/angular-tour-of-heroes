8ee7a0d757f9a0c24d33c460e2b076d24f6ce372
Ainara Saracho <ainara.saracho@gmail.com>
2021-03-22 20:01:32 +0100

    Añadir dashboar a la aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 /* Rutas que queremos que existan en la aplicación
    Cada ruta consta de un path y su componente de destino */
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, /* Ruta por defecto */
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-
 import { FormsModule } from '@angular/forms';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
