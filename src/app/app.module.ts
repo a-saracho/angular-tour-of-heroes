2603fe82540b4d9128d9000eb7a6e4f7759dc64b
Ainara Saracho <ainara.saracho@gmail.com>
2021-03-23 19:30:19 +0100

    Obtenemos los datos mediante una llamada GET al servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 /*Para utilizar ngModule en los inputs para modificar propiedades*/
 import { FormsModule } from '@angular/forms';
-import { MessagesComponent } from './messages/messages.component';
+/* Para utilizar rutas personalizadas*/
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+/*Para hacer peticiones http*/
+import { HttpClientModule } from '@angular/common/http';
+
+/* Para simular peticiones http a un servidor, en realidad nos comunicamos con un componente */
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +29,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService ),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+/* Para hacer peticiones http */
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
@@ -11,18 +14,24 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
-  /* Recibimos/enviamos los datos de forma síncrona */
-  /*getHeroes(): Hero[] {
-    return HEROES;
-  }*/
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+    /* Recibimos/enviamos los datos de forma síncrona */
+    /*getHeroes(): Hero[] {
+      return HEROES;
+    }*/
+    private heroesUrl = 'api/heroes';  // URL to web api
+
 
   /* Recibimos/enviamos los datos de forma asíncrona y usamos observable
       ya que la petición http al servidor nos devolverá los datos como observable*/
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
+    /*const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return heroes;*/
+    /* Hacemos una petición GET mediante HTTP al servidor REST */
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   /* Función para buscar un hero por su id */
   getHero(id: number): Observable<Hero> {
@@ -31,5 +40,10 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id) as Hero;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+  /* Log a HeroService message with the MessageService
+     (para no llamar a message service directamente todo el rato) */
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
